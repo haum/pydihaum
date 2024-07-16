049b14da6059c97ca8bc1bbca95284ac08ccc2bb
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2024-07-17 00:01:27 +0200

    mqtt: redo the wiring in mqtt to allow access depending on the ACL per user

--- pydihaum/idihaum/mqtt.py
+++ pydihaum/idihaum/mqtt.py
@@ -14,7 +14,7 @@
 from django.conf import settings
 import paho.mqtt.client as mqtt
 
-from .models import Card, User, Log
+from .models import Card, User, Log, Access_reader
 
 
 def on_connect(mqtt_client, userdata, flags, rc):
@@ -27,23 +27,31 @@ def on_connect(mqtt_client, userdata, flags, rc):
 
 def on_message(mqtt_client, userdata, msg):
    print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
-   if msg.topic == 'haum/gachaum/tag/uid':
+   try:
+       reader = Access_reader.objects.get(listen_topic=msg.topic)
+   except Access_reader.DoesNotExist:
+       return
+   if reader:
        uid = msg.payload.decode('utf-8')
        card = Card.objects.filter(uid=uid).first()
        if card:
            user = card.user
            if not card.active:
-               Log(user=user, card=card, comment='Denied: inactive card').save()
+               Log(user=user, card=card, reader=reader, comment='Denied: inactive card').save()
+           elif not reader.active:
+               Log(reader=reader, user=user, card=card, comment='Denied: inactive reader').save()
            else:
                if user and not user.active:
-                   Log(user=user, card=card, comment='Denied: inactive user').save()
+                   Log(user=user, card=card, reader=reader, comment='Denied: inactive user').save()
+               elif user.allowed_accesses.filter(id=reader.id).count() == 0:
+                   Log(user=user, card=card, reader=reader, comment='Denied: access forbidden').save()
                else:
-                   Log(user=user, card=card).save()
-                   mqtt_client.publish('haum/gachaum/strike', 'open')
-                   print(f'User {user} logged in')
+                   Log(user=user, card=card, reader=reader).save()
+                   mqtt_client.publish(reader.answer_topic, reader.message_topic)
+                   print(f'Access granted for {user} on reader {reader}')
        else:
            print(f'No card found for uid {uid}')
-           Log(unknown_card=uid).save()
+           Log(unknown_card=uid, reader=reader).save()
 
 def initialize_mqtt():
     client = mqtt.Client()
